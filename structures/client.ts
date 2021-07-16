45b225040da572740ea71a90b7999c99643d0453
joe27g <20873239+joe27g@users.noreply.github.com>
2021-07-16 16:57:51 -0500

    change stuff that may or may not be used

--- structures/client.ts
+++ structures/client.ts
@@ -3,26 +3,29 @@ import path from 'path';
 
 export default class Client {
     utils: Record<string, any>;
+    events: Record<string, any>;
 
     constructor() {
-        this.loadUtils();
+        this.utils = this.loadFiles('utils');
+        this.events = this.loadFiles('events');
     }
 
-    loadUtils() {
-        this.utils = {};
-        // load and validate utils
-        const utilFiles = fs.readdirSync('./utils');
-        for (const i in utilFiles) {
-            if (!utilFiles[i].endsWith('.ts')) continue;
-            let utilFile;
-            const eName = utilFiles[i].replace(/\.ts$/, '');
+    loadFiles(folderName, extension = 'ts') {
+        const fileObj = {};
+        const files = fs.readdirSync(`../${folderName}`);
+        for (const i in files) {
+            if (!files[i].endsWith(extension)) continue;
+            let file;
+            const extensionRegex = new RegExp(`\\.${extension}$`);
+            const nameWithoutExtension = files[i].replace(extensionRegex, '');
             try {
-                utilFile = require(path.join(__dirname, 'utils', eName));
-                this.utils[eName] = utilFile;
+                file = require(path.join(__dirname, 'utils', nameWithoutExtension));
+                fileObj[nameWithoutExtension] = file;
             }
             catch (err) {
-                console.error(`Failed to load util ${utilFiles[i]}: ${err}\n${err.stack}`);
+                console.error(`Failed to load ${folderName} file ${files[i]}: ${err}\n${err.stack}`);
             }
         }
+        return fileObj;
     }
 }
