a05b8eb38a7a5a0437443ee8b2520bf62fc38af6
joe27g <20873239+joe27g@users.noreply.github.com>
2021-09-02 05:11:12 -0500

    add snowflake conversion

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,10 +1,13 @@
 import { GatewayParams } from "./gatewayTypes";
 import { GATEWAY_VERSIONS } from "./numberTypes";
 
+/***** bot options *****/
 
-export const API_BASE = "https://discord.com/api/v9";
+export const DEBUG = true;
 
-export const GATEWAY_ERROR_RECONNECT = true;
+export const USE_CACHE = true;
+
+export const GATEWAY_ERROR_RECONNECT = true; // whether to retry when failing to connect to gateway
 
 export const GATEWAY_ERROR_RECONNECT_TIMEOUT = 30000; // how many ms to wait when failing to connect to gateway
 
@@ -13,6 +16,8 @@ export const GATEWAY_PARAMS: GatewayParams = {
     encoding: 'json'
 }
 
-export const DEBUG = true;
+/***** general constants *****/
 
-export const USE_CACHE = true;
+export const API_BASE = "https://discord.com/api/v9";
+
+export const DISCORD_EPOCH = 1420070400000;
--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,9 +1,22 @@
+import { DISCORD_EPOCH } from "../data/constants";
+import { Snowflake, UnixTimestamp } from "../data/types";
+
 export default class MiscUtils {
-    static parseQueryString(data: Record<string, string | number>) {
+    static parseQueryString(data: Record<string, string | number>): string {
         let qs = '';
         for (const key in data) {
             qs += `${qs ? '&' : '?'}${key}=${encodeURIComponent(data[key])}`;
         }
         return qs;
+    }
+
+    // https://discord.com/developers/docs/reference#snowflakes-snowflake-id-format-structure-left-to-right
+    static snowflakeToTimestamp(id: Snowflake): UnixTimestamp {
+        const idInt = BigInt(id);
+        return Number(idInt >> BigInt(22)) + DISCORD_EPOCH;
+    }
+    // https://discord.com/developers/docs/reference#snowflake-ids-in-pagination-generating-a-snowflake-id-from-a-timestamp-example
+    static timestampToSnowflake(timestamp: UnixTimestamp) {
+        return (timestamp - DISCORD_EPOCH) << 22;
     }
 }
