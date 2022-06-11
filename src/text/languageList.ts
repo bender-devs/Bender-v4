76e33d3fd61f5634ce5f73b5fe325beef354112a
Mark <46086198+Awaydays@users.noreply.github.com>
2022-06-11 23:58:42 +0200

    [UNFINISHED] Add Dutch translation
    Commands need to be used to see how this will actually look/determine whether grammar or words need to be changed.

--- src/text/languageList.ts
+++ src/text/languageList.ts
@@ -1,9 +1,11 @@
 import * as en_US from './en-US.json';
+import * as nl_NL from './nl.json';
 import * as types from '../data/types';
 
 export type LangKey = keyof typeof en_US;
 
 const languages: types.LangMap = {
-    'en-US': en_US
+    'en-US': en_US,
+    'nl': nl_NL
 };
 export default languages;
