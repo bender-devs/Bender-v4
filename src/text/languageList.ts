dc95edef88917dc97788d2428db8f87ceb5df58e
Andreas B <6439218+YouKnowBlom@users.noreply.github.com>
2022-06-12 03:02:17 +0200

    Add Swedish translation

--- src/text/languageList.ts
+++ src/text/languageList.ts
@@ -1,4 +1,5 @@
 import * as en_US from './en-US.json';
+import * as sv_SE from './sv-SE.json';
 import * as nl_NL from './nl.json';
 import * as types from '../data/types';
 
@@ -6,6 +7,7 @@ export type LangKey = keyof typeof en_US;
 
 const languages: types.LangMap = {
     'en-US': en_US,
-    'nl': nl_NL
+    'nl': nl_NL,
+    'sv-SE': sv_SE
 };
 export default languages;
