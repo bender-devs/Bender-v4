0b042d4b28c51557e6dc7372665dafeb8215180a
snipsy <snipsy@rick-roll.me>
2022-06-16 20:51:20 +0200

    add german language

--- src/text/languageList.ts
+++ src/text/languageList.ts
@@ -1,11 +1,13 @@
 import * as en_US from './en-US.json';
 import * as nl_NL from './nl.json';
+import * as de from './de.json';
 import * as types from '../data/types';
 
 export type LangKey = keyof typeof en_US;
 
 const languages: types.LangMap = {
     'en-US': en_US,
-    'nl': nl_NL
+    'nl': nl_NL,
+    'de': de,
 };
 export default languages;
